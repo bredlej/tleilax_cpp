0087024ed8c6addc78df8393b83c06bdaca11b79
Patryk Szczypien <patryk.szczypien@gmail.com>
2022-03-19 11:57:31 +0100

    TLEILAX-15: - added Graph class - added unit test suite for graphs

---
+++ include/graph.h
@@ -0,0 +1,30 @@
+//
+// Created by geoco on 19.03.2022.
+//
+
+#ifndef TLEILAX_GRAPH_H
+#define TLEILAX_GRAPH_H
+
+#include <unordered_map>
+#include <vector>
+
+template<typename NodeT, typename DistanceT, typename HashFuncT, typename EqualFuncT>
+class Graph {
+    using AdjacencyList = std::unordered_map<NodeT, std::vector<std::pair<NodeT, DistanceT>>, HashFuncT, EqualFuncT>;
+
+public:
+    void add_edge(NodeT source_node, NodeT target_node, DistanceT distance, bool is_bidirectional);
+    AdjacencyList get() { return adjacency_list; };
+
+private:
+    AdjacencyList adjacency_list;
+};
+
+template<typename T, typename D, typename H, typename E>
+void Graph<T, D, H, E>::add_edge(T source_node, T target_node, D distance, bool is_bidirectional) {
+    adjacency_list[source_node].push_back(std::make_pair(target_node, distance));
+    if (is_bidirectional) {
+        adjacency_list[target_node].push_back(std::make_pair(source_node, distance));
+    }
+}
+#endif//TLEILAX_GRAPH_H
---
+++ test/test_graph.cpp
@@ -0,0 +1,60 @@
+//
+// Created by geoco on 19.03.2022.
+//
+#include <gtest/gtest.h>
+#include <cstdio>
+#include <graph.h>
+
+struct Node {
+    explicit Node(int number) : number(number) {};
+    int number;
+    bool operator==(const Node other) const {
+        return number == other.number;
+    }
+};
+struct NodeHash {
+    std::size_t operator() (const Node node) const {return node.number;};
+};
+
+struct NodeEqual {
+    bool operator() (const Node first, const Node second) const {return first.number == second.number ;};
+};
+
+class TleilaxGraphTest : public testing::Test {
+protected:
+    TleilaxGraphTest() = default;
+};
+
+TEST_F(TleilaxGraphTest, RunsTest) {
+    EXPECT_TRUE(true);
+}
+
+TEST_F(TleilaxGraphTest, CreatesGraphWithTwoNodesNonDirectional) {
+    Graph<Node, int, NodeHash, NodeEqual> graph;
+    graph.add_edge(Node{0}, Node {1}, 1, false);
+    auto adjacency_list = graph.get();
+
+    EXPECT_EQ(1, adjacency_list.size());
+    EXPECT_EQ(1, adjacency_list[Node{0}].size());
+    EXPECT_EQ(1, adjacency_list[Node{0}][0].first.number);
+    EXPECT_EQ(1, adjacency_list[Node{0}][0].second);
+}
+
+TEST_F(TleilaxGraphTest, CreatesGraphWithTwoNodesBiDirectional) {
+    Graph<Node, int, NodeHash, NodeEqual> graph;
+    graph.add_edge(Node{0}, Node {1}, 1, true);
+    auto adjacency_list = graph.get();
+
+    EXPECT_EQ(2, adjacency_list.size());
+    EXPECT_EQ(1, adjacency_list[Node{0}].size());
+    EXPECT_EQ(1, adjacency_list[Node{0}][0].first.number);
+    EXPECT_EQ(1, adjacency_list[Node{0}][0].second);
+    EXPECT_EQ(1, adjacency_list[Node{1}].size());
+    EXPECT_EQ(0, adjacency_list[Node{1}][0].first.number);
+    EXPECT_EQ(1, adjacency_list[Node{1}][0].second);
+}
+
+int main(int ac, char *av[]) {
+    testing::InitGoogleTest(&ac, av);
+    return RUN_ALL_TESTS();
+}
