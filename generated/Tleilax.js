d93f093754a8d361eb96a0db60aee59af111ff20
bredlej <patryk.szczypien@gmail.com>
2022-04-19 08:15:19 +0000

    Deploying to gh-pages from @ bredlej/tleilax_cpp@aea9da7cf4e6a524c7f2bd5479b2e58b90613ac7 🚀

--- generated/Tleilax.js
+++ generated/Tleilax.js
@@ -198,7 +198,7 @@ Module['FS_createPath']("/assets", "shaders", true, true);
     }
 
     }
-    loadPackage({"files": [{"filename": "/assets/json/ship_components.json", "start": 0, "end": 1384}, {"filename": "/assets/json/ships.json", "start": 1384, "end": 1934}, {"filename": "/assets/shaders/base.fs", "start": 1934, "end": 2420}, {"filename": "/assets/shaders/base_lightning_instanced.vs", "start": 2420, "end": 3250}, {"filename": "/assets/shaders/lighting.fs", "start": 3250, "end": 5262}], "remote_package_size": 5262, "package_uuid": "e1ab602d-2944-4a41-bd28-acd9b679bda7"});
+    loadPackage({"files": [{"filename": "/assets/json/ship_components.json", "start": 0, "end": 1384}, {"filename": "/assets/json/ships.json", "start": 1384, "end": 1934}, {"filename": "/assets/shaders/base.fs", "start": 1934, "end": 2420}, {"filename": "/assets/shaders/base_lightning_instanced.vs", "start": 2420, "end": 3250}, {"filename": "/assets/shaders/lighting.fs", "start": 3250, "end": 5262}], "remote_package_size": 5262, "package_uuid": "ef3820ee-1ff7-42d8-80ed-2ff792e2ee9c"});
 
   })();
 
