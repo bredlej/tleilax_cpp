e5035d680e2e55ebea03686a53343167be06624a
bredlej <patryk.szczypien@gmail.com>
2022-04-24 17:22:50 +0000

    Deploying to gh-pages from @ bredlej/tleilax_cpp@55ba2f5c130b789dd19b21812598859a78dbf115 🚀

--- generated/Tleilax.js
+++ generated/Tleilax.js
@@ -199,7 +199,7 @@ Module['FS_createPath']("/assets", "shaders", true, true);
     }
 
     }
-    loadPackage({"files": [{"filename": "/assets/names/indian", "start": 0, "end": 330}, {"filename": "/assets/names/greek", "start": 330, "end": 725}, {"filename": "/assets/json/ship_components.json", "start": 725, "end": 2109}, {"filename": "/assets/json/ships.json", "start": 2109, "end": 2659}, {"filename": "/assets/shaders/base.fs", "start": 2659, "end": 3145}, {"filename": "/assets/shaders/base_lightning_instanced.vs", "start": 3145, "end": 3975}, {"filename": "/assets/shaders/lighting.fs", "start": 3975, "end": 5987}], "remote_package_size": 5987, "package_uuid": "74dec312-1101-4e14-9424-24ec3de58e2a"});
+    loadPackage({"files": [{"filename": "/assets/names/indian", "start": 0, "end": 330}, {"filename": "/assets/names/greek", "start": 330, "end": 725}, {"filename": "/assets/json/ship_components.json", "start": 725, "end": 2109}, {"filename": "/assets/json/ships.json", "start": 2109, "end": 2659}, {"filename": "/assets/shaders/base.fs", "start": 2659, "end": 3145}, {"filename": "/assets/shaders/base_lightning_instanced.vs", "start": 3145, "end": 3975}, {"filename": "/assets/shaders/lighting.fs", "start": 3975, "end": 5987}], "remote_package_size": 5987, "package_uuid": "cbbc2e05-0da2-43a5-a811-ecfea07787b2"});
 
   })();
 
